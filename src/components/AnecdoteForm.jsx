1c68f64c3412485090e1a6cc4137a811f197de6b
2underscores <jeremy__smith@hotmail.com>
2024-12-04 08:23:59 +1100

    Creation saved to json DB

--- src/components/AnecdoteForm.jsx
+++ src/components/AnecdoteForm.jsx
@@ -1,17 +1,20 @@
 import { useDispatch } from 'react-redux'
 import { addAnecdote } from '../reducers/anecdoteReducer'
 import { useNotification } from '../hooks/useNotification'
+import { create as anecdoteServiceCreate } from '../services/anecdotes'
 
 
 const AnecdoteForm = () => {
     const dispatch = useDispatch()
     const notify = useNotification()
 
-    const submitCreateNote = (e) => {
+    const submitCreateNote = async (e) => {
         e.preventDefault()
         const anecdoteContent = new FormData(e.target).get('newAnecdote')
-        dispatch(addAnecdote(anecdoteContent))
-        notify(`Added Anecdote: ${anecdoteContent}`)
+        const newAnecdote = await anecdoteServiceCreate(anecdoteContent)
+        console.log({newAnecdote})
+        dispatch(addAnecdote(newAnecdote))
+        notify(`Added Anecdote: ${newAnecdote}`)
         e.target.reset()
     }
     return (
--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -1,14 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit"
 
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
   initialState: [],
@@ -21,7 +12,7 @@ const anecdoteSlice = createSlice({
       })
     },
     addAnecdote: (state, action) => {
-      state.push(asObject(action.payload)) // FIXME: remove object formatting
+      state.push(action.payload)
     },
     setAnecdotes: (state, action) => {
       return action.payload
